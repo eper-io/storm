a7a3b22c3d5628c3d57b1374ec64085cd70330bb
Miklos Szegedi <hq@opensource.eper.io>
2024-05-22 21:04:56 -0700

    work

--- data/data.go
+++ data/data.go
@@ -23,7 +23,7 @@ import (
 // You should have received a copy of the CC0 Public Domain Dedication along with this document.
 // If not, see https://creativecommons.org/publicdomain/zero/1.0/legalcode.
 
-var MemCache = "https://hour.schmied.us"
+var MemCache = "https://localhost.schmied.us"
 var BlockList = make([]string, 0)
 var LastSnapshot = ""
 
