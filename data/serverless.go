0adcc69ac52cfd78744a0a7d1ca61528b79532a1
Miklos Szegedi <hq@portal.eper.io>
2024-06-09 09:23:01 -0700

    work

--- data/serverless.go
+++ data/serverless.go
@@ -12,6 +12,13 @@ import (
 	"time"
 )
 
+// This document is Licensed under Creative Commons CC0.
+// To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights
+// to this document to the public domain worldwide.
+// This document is distributed without any warranty.
+// You should have received a copy of the CC0 Public Domain Dedication along with this document.
+// If not, see https://creativecommons.org/publicdomain/zero/1.0/legalcode.
+
 func RunShardClient(instructions io.Reader, done *sync.WaitGroup, handler http.HandlerFunc) {
 	implementation := bufio.NewScanner(instructions)
 	for implementation.Scan() {
@@ -30,7 +37,7 @@ func RunShardClient(instructions io.Reader, done *sync.WaitGroup, handler http.H
 				time.Sleep(10 * time.Second)
 				current := ServerlessGet(api)
 				if !bytes.Equal(current, snapshot) {
-					fmt.Println(string(current), string(snapshot))
+					//fmt.Println(string(current), string(snapshot))
 					done.Done()
 					return
 				}
